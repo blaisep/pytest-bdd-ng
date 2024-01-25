e03f5aa5e347ed425c3de0c20b3b46dc7d08e8b0
blaisep <blaise@gmail.com>
2024-01-24 21:29:25 -0800

    WIP matrix build

--- ops/settings.py
+++ ops/settings.py
@@ -4,20 +4,26 @@ Settings for CI, test, build, etc.
 Consolidate the settings into a single python file from the various existing file formats.
 from .github/workflows/release.yaml
 """
-
-from dotenv import dotenv_values
-env_vars = dotenv_values(".env")
+import os
+# from dotenv import dotenv_values
+# env_vars = dotenv_values(".env")   # Keep our secrets out of source control
+dir_source = os.path.abspath("../src")
+dir_tests = os.path.abspath("../tests")
 
 # Git copies these repos for each action, no caching from one run to another
-ci_environment = client.git("https://github.com/cucumber/ci-environment.git").commit(
-    "e6acaf0b83d8eab649eb0bdc9ef2d6e18c14324e").tree()
-compatibility = client.git("https://github.com/cucumber/compatibility-kit.git").commit(
-    "2fd68be7be09eadee05a02badbf3191be9a27270").tree()
-gherkin = client.git("https://github.com/cucumber/gherkin.git").commit(
-    "5ec532537888e41c77dd1dd6ace25a72a44cdf0e").tree()
-messages = client.git("https://github.com/elchupanebrej/messages").commit(
-    "cecd1fed3d47d59af7f0623f148f6752c9e93ee2").tree()
+ci_environment = dict(url="https://github.com/cucumber/ci-environment.git",
+                      commit="e6acaf0b83d8eab649eb0bdc9ef2d6e18c14324e"),
+compatibility = dict(url="https://github.com/cucumber/compatibility-kit.git",
+                     commit="2fd68be7be09eadee05a02badbf3191be9a27270"),
+gherkin = dict(url="https://github.com/cucumber/gherkin.git",
+               commit="5ec532537888e41c77dd1dd6ace25a72a44cdf0e"),
+messages = dict(url="https://github.com/elchupanebrej/messages",
+                commit="cecd1fed3d47d59af7f0623f148f6752c9e93ee2")
 
+gitmodules = [ci_environment,
+              compatibility,
+              gherkin,
+              messages]
 # github creates this matrix from scratch for each action.
 python_version = [
     "3.8",
@@ -30,11 +36,9 @@ python_version = [
     "pypy3.10",
     ]
 
-os = {
-    "ubuntu" : "latest",
-    "windows" : "latest",
-    "macos" : "latest",
-    }
+os = dict(ubuntu="latest",
+    windows="latest",
+    macos="latest")
 
 # let's make it easy for legacy references to {{ matrix.python_version }}' etc
-matrix = [python_version, os]
+matrix = [os, python_version]
